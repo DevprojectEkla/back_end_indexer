18eb3f35d191320576c71acf55ad52ef57596e1e
ekla <barlavigabriel@gmail.com>
2023-10-30 02:49:09 +0100

    unbelievable we manage to factorize the tf_to_json function into a generic hashmap_to_json function so that we can call it on every hashmap possible and generate a json. TODO:Implement this to generate the index.json file which is <PathBuf, TermFreq>

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@ use std::{
     collections::HashMap,
     ffi::OsStr,
     fs::{self, metadata, DirEntry, File, Metadata, ReadDir},
-    io::Result,
-    path::Path,
+    io::{Result, Write},
+    path::{Path, PathBuf},
     process::exit,
     thread,
     time::Duration,
 };
 
 use walkdir::WalkDir;
+///Ces types servent seulement à éviter des répétitions un peu lourde de generic types
+type TermFreq = HashMap<String, usize>;
+type IndexDoc = HashMap<PathBuf, TermFreq>;
+type SliceContent = Vec<char>;
 
 #[derive(Debug, Clone)]
 struct Lexer<'a> {
@@ -97,26 +101,99 @@ impl<'a> Iterator for Lexer<'a> {
         self.next_token()
     }
 }
-fn index_document(content: &Vec<char>) -> HashMap<String, usize> {
-    let mut hashmap: HashMap<String, usize> = HashMap::new();
-    for token in Lexer::new(content) {
-        // println!("Content: {content:?}\n");
-        println!("\nTOKEN: {token:?}");
-        println!("=> {:?}", token.iter().collect::<String>());
-        hashmap.insert(token.iter().collect::<String>(), token.len());
+///cette fonction est typiquement appelée avec comme argument walk_dir(path) qui retourne un Vec<String> et permet d'indexer tous les fichiers
+fn index_all(list_path: Vec<String>) -> () {
+    let mut n = 1;
+    for path in list_path {
+        let content = check_file_type_and_send_to_parser(path.as_str());
+        println!("Indexing {path}...");
+        let index = index_document(&content);
+        let file_name = Path::new(&path)
+            .file_stem()
+            .expect("there should be a stem to this path")
+            .to_str()
+            .expect("conversion to &str must be ok")
+            .trim_matches('"');
+        // let file_name = path
+        //     .split("/")
+        //     .collect::<Vec<&str>>()
+        //     .last()
+        //     .expect("it should be a file name")
+        //     .to_string()
+        //     .split(".")
+        //     .collect::<Vec<&str>>()[0]
+        //     .to_string();
+        tf_to_json(index, file_name);
+        n += 1;
+        // println!("content:{content:?}\nindex: {index:?} ");
     }
-    println!("{:?}", hashmap);
-    hashmap
+}
+///cette fonction est appelée par index_all pour effectivement indexer les fichiers. C'est elle qui
+///réalise l'indexation d'un content &Vec<char> retourné par la fonction du parser correspondant au
+///type de fichier. Elle utilise le Lexer pour générer les token des différents termes du document
+///et utilise un HashMap (~un dictionnaire) pour stocker les données (terme ,fréquence) pour chaque token. Elle est aussi responsable du calcul de la fréquence des terme. TODO: faire une fonction distincte pour cette dernière opération?
+
+fn sort_hashmap<K, V>(hashmap: HashMap<K, V>) -> Vec<(String, usize)> {
+    todo!()
+}
+
+fn sort_tf_hashmap(hashmap: &TermFreq) -> Vec<(&String, &usize)> {
+    let mut stats = hashmap.into_iter().collect::<Vec<_>>(); // returns [("key1", value1),...,]
+    stats.sort_by_key(|(_, freq)| *freq);
+    //unstable_by_key(|x| (*x).1);
+    stats.reverse();
+    stats
+}
+
+fn index_document(content: &SliceContent) -> TermFreq {
+    let mut tf: TermFreq = HashMap::new();
+    for token in Lexer::new(content) {
+        let term = token.iter().collect::<String>();
+        if let Some(freq) = tf.get_mut(&term) {
+            *freq += 1;
+        } else {
+            tf.insert(term, 1);
+        }
+        // println!("Content: {content:?}\n");
+        // println!("\nTOKEN: {token:?}");
+        // println!("=> {:?}", token.iter().collect::<String>());
+        // hashmap.insert(token.iter().collect::<String>(), token.len());
+    }
+    // let mut stats = tf.iter().collect::<Vec<_>>();
+    // stats.sort_by_key(|(_, freq)| *freq);
+    // stats.reverse();
+
+    // for (t, f) in stats.iter().take(10) {
+    //     println!("term {t} => {f} times")
+    // }
+    // let sorted = sort_hashmap(tf);
+    // println!("{:?}", sorted.iter().take(10));
+    // thread::sleep(Duration::from_secs(3));
+    // sorted
+    //     .into_iter()
+    //     .map(|(key, value)| (key.to_string(), value))
+    //     .collect()
+    return tf;
+    // println!("{:?}", tf);
+    // let sorted = sort_hashmap(tf);
+    // sorted
+    //     .into_iter()
+    //     .map(|(key, value)| (key.to_string(), value))
+    //     .collect()
 
     // todo!("not implemented yet")
 }
-
-fn check_file_type_and_send_to_parser(path: &str) -> Vec<char> {
+///cette fonction est appelée par la fonction index_all(). C'est elle qui redirige un fichier vers
+///son parser spécifique en vu de récupérer un Vec<char> qui est en gros le contenu du fichier avant
+///tokenisation c'est le retour de cette fonction qui est (passé sous la forme d'une référence) à
+///index_document()
+fn check_file_type_and_send_to_parser(path: &str) -> SliceContent {
     let extension = Path::new(path).extension().expect("should be an extension");
     //.extension() returns an Enum Option (None, Some)
     match extension.to_str() {
         Some("pdf") => parse_pdf_file(path),
         Some("xml") => parse_xml_file(path),
+        Some("txt") => parse_txt_file(path),
         Some(&_) => parse_other_files(path),
         None => parse_no_extension(path),
     }
@@ -124,84 +201,90 @@ fn check_file_type_and_send_to_parser(path: &str) -> Vec<char> {
 /// this is the function that iterate on each directory to retrieve all files of the whole tree
 /// starting from path which should logically but not necessarily be a path to a directory. On each
 /// files it calls the function that will check the extension and send the path file to the parser
-fn walk_dir(path: &str) -> Result<()> {
+fn walk_dir(path: &str) -> Vec<String> {
     let mut i = 1;
     let mut n_files = 1;
+    let mut list_path: Vec<String> = Vec::new();
     for entry in WalkDir::new(path) {
-        i += 1;
-        let entry = entry?.clone();
+        let entry = entry
+            .expect("WalkDir doit bien donner une valeur pour entry")
+            .clone();
         if entry.path().is_file() {
-            let content =
-                check_file_type_and_send_to_parser(entry.path().to_str().expect("should match"));
-            let index = index_document(&content);
+            let path_to_file = entry.path().to_str().expect("should match");
+            list_path.push(path_to_file.to_string());
 
-            println!("number of iteration: {i}, number of file:  {n_files} content:{content:?}\nindex: {index:?} ");
+            // let content =
+            //     check_file_type_and_send_to_parser(entry.path().to_str().expect("should match"));
+            // let index = index_document(&content);
+
             n_files += 1;
-            let pause = time::Duration::from_secs(1);
-            thread::sleep(pause);
+            i += 1;
+            // let pause = time::Duration::from_secs(1);
+            // thread::sleep(pause);
         }
     }
-    Ok(())
+    println!("number of file:  {n_files} ");
+    list_path
 }
+
 fn check_is_dir(path_dir: &Path) -> Result<bool> {
     let meta = metadata(path_dir)?.file_type().is_dir();
 
     Ok(meta)
 }
 
-// fn check_is_pdf(path: &str) -> bool {
-//     let extension = Some(OsStr::new("pdf"));
-//     if Path::new(path).extension() == extension {
-//         return true;
-//     } else {
-//         false
-//     }
-// }
-fn parse_no_extension(path: &str) -> Vec<char> {
+fn parse_no_extension(path: &str) -> SliceContent {
     todo!("no extension parser to implement to parse {path}");
 }
-fn parse_other_files(path: &str) -> Vec<char> {
+fn parse_other_files(path: &str) -> SliceContent {
     todo!("other parser to implement to parse {path}");
 }
-fn parse_xml_file(path: &str) -> Vec<char> {
+fn parse_txt_file(path: &str) -> SliceContent {
+    let content = fs::read_to_string(path).expect("we should be able to open a .txt file");
+    let size = format_size(content.len());
+    let content = content.chars().collect();
+    println!("file:{path} => size:{size}");
+
+    return content;
+}
+fn parse_xml_file(path: &str) -> SliceContent {
     todo!("xml parser to implement to parse {path}");
 }
-fn parse_pdf_file(path: &str) -> Vec<char> {
+fn parse_pdf_file(path: &str) -> SliceContent {
+    println!("Parsing PDF file:{path} ...");
     let pdf = poppler::PopplerDocument::new_from_file(path, "").unwrap_or_else(|err| {
         eprintln!("ERROR: could not read file {path}: {err}");
         exit(1)
     });
     let n_pages = pdf.get_n_pages();
     let mut contents = String::new();
-    let mut char_vec: Vec<char> = Vec::new();
+    let mut char_vec: SliceContent = Vec::new();
 
     for i in 0..n_pages {
         let page = pdf.get_page(i).expect(&format!(
             "get_page should workbut {i} might bee out of range"
         ));
         if let Some(content) = page.get_text() {
-            char_vec = content.chars().collect::<Vec<char>>();
+            char_vec = content.chars().collect::<SliceContent>();
             // for token in Lexer::new(&char_vec) {
             //     // println!("Content: {content}\nSplit Content:{char_vec:?}");
             //     println!("\nTOKEN: {token:?}");
             //     println!("=> {:?}", token.iter().collect::<String>());
             // }
             contents.push_str(content);
+
             // println!("{content:?}")
             let page_n = i + 1;
             println!(
-                "contents: {contents}, file:{path}, page {page_n}/{n_pages} => size:{size}",
+                "Parsing PDF pages page {page_n}/{n_pages} => size:{size} ...",
                 size = format_size(content.len())
             );
-            return char_vec;
-        } else {
-            return char_vec;
         }
     }
     if n_pages == 0 {
         return char_vec;
     }
-    char_vec
+    contents.chars().collect::<SliceContent>()
 }
 
 fn format_size(size: usize) -> String {
@@ -222,10 +305,52 @@ fn format_size(size: usize) -> String {
         format!("{:.2} TB", size as f64 / TB)
     }
 }
+fn tf_to_index(path: PathBuf, tf: TermFreq) -> IndexDoc {
+    let mut index_doc = IndexDoc::new();
+    index_doc.insert(path, tf);
+    index_doc
+}
+fn index_to_json(index: IndexDoc) {
+    todo!()
+}
+fn tf_to_json(tf: TermFreq, file_stem: &str) {
+    hashmap_to_json(&tf, file_stem, sort_tf_hashmap);
+}
+fn hashmap_to_json<K, V, F>(hashmap: &HashMap<K, V>, name: &str, mut sort_function: F)
+where
+    K: std::fmt::Debug,
+    V: std::fmt::Debug,
+    F: FnMut(&HashMap<K, V>) -> Vec<(&String, &usize)>,
+{
+    // let mut data = <Vec<_>>::new();
+    let data = sort_function(hashmap);
+    // for (key, value) in hashmap {
+    //     data.push(format!(r#""{:?}":"{:?}",\n"#, key, value));
+    // }
+    let json_format = format!(
+        r#"{{{}}}
+"#,
+        data.iter()
+            .map(|(k, v)| format!(r#""{:?}":"{:?}""#, k, v))
+            .collect::<Vec<_>>()
+            .join(",\n")
+    );
+
+    let mut json = File::create(format!("data/_index-{name}.json"))
+        .expect("we should be able to create a file on the system");
+    json.write_all(json_format.as_bytes())
+        .expect("could write to a simple file")
+}
 
 fn main() {
-    let _ = walk_dir("docs/numbers.pdf");
+    let list_files = walk_dir("docs/");
+    // thread::sleep(Duration::from_secs(1));
+    index_all(list_files);
+
     // let all_docs = HashMap::<Path, HashMap<String, usize>>::new();
     // let file_path = "docs/cours2.pdf";
+    // parse_pdf_file(file_path);
+    //     .map(|(key, value)| (key.to_string(), value))
+    //     .collect()
     // parse_pdf_file(file_path);
 }
